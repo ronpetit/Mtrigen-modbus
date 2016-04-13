f82638e972b8000a91735f144c78fa0997919375
ronpetit <elronaldpetit@gmail.com>
2016-04-13 08:13:21 -0500

    Modified two files to work together

--- readings.py
+++ readings.py
@@ -1,8 +1,7 @@
-from startDSE.py import RPMRegister
-from startDSE.py import DCRegister
+import startDSE
 import time
 
 while 1==1:
-time.sleep(2)
-print("Engine speed ", RPMRegister, " RPM")
-print("DC load current ", DCRegister, " A")
+	time.sleep(2)
+	print("Engine speed ", startDSE.RPMRegister, " RPM")
+	print("DC load current ", startDSE.DCRegister, " A")
