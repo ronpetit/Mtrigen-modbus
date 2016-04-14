d7e2853cebce7a65b8f44f2283508d88e3eeab67
ronpetit <elronaldpetit@gmail.com>
2016-04-14 18:28:57 -0500

    LOT OF CHANGES!

---
+++ Blower.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+
+print("Initializing")
+time.sleep(5)
+PWM = GPIO.PWM(12,1000)
+print("Running")
+while 1==1:
+	PWM.start(35)
+
+
