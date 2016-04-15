e051e30026f4c5394f7b7517a05282d9196619a3
Ron <elronaldpetit@gmail.com>
2016-04-15 08:46:35 -0500

    Update Monitor.py

--- Monitor.py
+++ Monitor.py
@@ -5,14 +5,14 @@ import ReadAndWriteRegisters as IO
 RPMPageNumber = 4
 RPMRegisterOffset = 6
 DCCurrentPageNumber = 4
-DCCurrentRegisterOffset = 207
+DCCurrentRegisterOffset = 205
 
 print("Starting the RPM and current monitor")
 time.sleep(2)
 while 1==1:
 	RPM = IO.read_register(RPMPageNumber, RPMRegisterOffset, 1)
 	print("Engine RPM ", RPM, " RPM")
-	DC = IO.read_register(DCCUrrentPageNumber, DCCurrentRegisterOffset, 0.1)
+	DC = IO.read_register(DCCurrentPageNumber, DCCurrentRegisterOffset, 0.1)
 	print("Alternator DC current ", DC, " A")
 	time.sleep(2)
 
